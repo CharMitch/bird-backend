1e7b96ea2058a2b3accb05b6e28d900c6dcba8e2
Charlene Mitchell <charlenemitchell@charlenes-air.lan>
2023-10-09 21:46:05 -0400

    worked on bird db

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,17 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('black-capped Chickadee', '2.0', '02-03-2017', '43081', 'bcchickadee.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('Pacific Wren', '15.0', '01-16-2018', '85138', 'pacificwren.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('American Robin', '213.0', '04-11-2020', '45005', 'americanrobin.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('Bufflehead', '21.0', '11-21-2015', '98230', 'bufflehead.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('Red-bellied Woodpecker', '6.0', '03-12-2017', '02741', 'redbelliedwoodpecker.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('Northern Cardinal', '45.0', '06-02-2019', '78585', 'northerncardinal.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('Eastern Screech-Owl', '4.0', '09-10-2020', '76570', 'easternscreechowl.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('Belted Kingfisher', '13.0', '01-11-2019', '27929', 'beltedkingfisher.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('Carolina Wren', '17.0', '12-26-2019', '45011', 'carolinawren.jpg');
+INSERT INTO birds (bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image ) VALUES ('Anna`s Hummingbird', '6.0', '03-02-2013', '95476', 'annashummingbird.jpg');
+
+
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,14 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE birds (
+	bird_id SERIAL,
+	bird_name varchar(50) NOT NULL UNIQUE,
+	number_of_sightings decimal(10) NOT NULL,
+	most_recent_sighting date,
+	zip_code varchar (20) NOT NULL,
+	bird_image varchar(300) NOT NULL,
+	CONSTRAINT PK_bird PRIMARY KEY (bird_id)
+);
+
 COMMIT TRANSACTION;
