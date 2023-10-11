159f789962d8b41b2418a73ea4e2758390bdcae8
Charlene Mitchell <charlenemitchell@charlenes-air.lan>
2023-10-10 20:58:31 -0400

    SQL for get bird by ID

--- java/src/main/java/com/techelevator/controller/BirdController.java
+++ java/src/main/java/com/techelevator/controller/BirdController.java
@@ -1,20 +1,28 @@
 package com.techelevator.controller;
 
 
+
 import com.techelevator.dao.BirdDao;
+import com.techelevator.dao.UserDao;
 import com.techelevator.model.Bird;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
+// @PreAuthorize("isAuthenticated()")
+
 public class BirdController {
 
 
     private BirdDao birdDao;
 
+    private UserDao userDao;
 
-    public BirdController(BirdDao birdDao) {
+
+    public BirdController(BirdDao birdDao, UserDao userDao) {
         this.birdDao = birdDao;
+        this.userDao = userDao;
     }
 
 
--- java/src/main/java/com/techelevator/dao/JdbcBirdDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcBirdDao.java
@@ -2,6 +2,7 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Bird;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import java.util.List;
 
@@ -15,9 +16,14 @@ public class JdbcBirdDao implements BirdDao {
 
     public Bird getBirdById(int birdId) {
         Bird bird = null;
-        String sql = "";
+        String sql = "SELECT bird_id, bird_name, number_of_sightings, most_recent_sighting, zip_code, bird_image " +
+                "FROM birds " +
+                "WHERE bird_id = ?";
 
-
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, birdId);
+        if (results.next()) {
+            bird = mapRowToBird(results);
+        }
 
         return bird;
     }
@@ -32,4 +38,21 @@ public class JdbcBirdDao implements BirdDao {
 //
 //        return birdsByZip;
 //    }
+
+
+
+
+
+    private Bird mapRowToBird(SqlRowSet rs) {
+        Bird bird = new Bird();
+        bird.setBird_id(rs.getInt("bird_id"));
+        bird.setBird_name(rs.getString("bird_name"));
+        bird.setNumber_of_sightings(rs.getInt("number_of_sightings"));
+        bird.setMost_recent_sighting(rs.getDate("most_recent_sighting"));
+        bird.setZip_code(rs.getInt("zip_code"));
+        bird.setBird_image(rs.getString("bird_image"));
+
+        return bird;
+    }
+
 }
